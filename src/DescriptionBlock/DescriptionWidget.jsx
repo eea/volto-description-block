f5ca547cd1e7f02513ef63b62f993d150819aff1
Miu Razvan <miu.razvan28@gmail.com>
2023-05-19 08:41:55 +0200

    fix: add description widget on id if the field is added by behavior

--- src/DescriptionBlock/DescriptionWidget.jsx
+++ src/DescriptionBlock/DescriptionWidget.jsx
@@ -8,7 +8,112 @@ import PropTypes from 'prop-types';
 import { Label, TextArea } from 'semantic-ui-react';
 
 import { injectIntl } from 'react-intl';
+import config from '@plone/volto/registry';
 import { FormFieldWrapper } from '@plone/volto/components';
+
+/**
+ * Get default widget
+ * @method getViewDefault
+ * @returns {string} Widget component.
+ */
+const getWidgetDefault = () => config.widgets.default;
+
+/**
+ * Get widget by factory attribute
+ * @method getWidgetByFactory
+ * @param {string} id Id
+ * @returns {string} Widget component.
+ */
+const getWidgetByFactory = (factory) =>
+  config.widgets.factory?.[factory] || null;
+
+/**
+ * Get widget by field's `widget` attribute
+ * @method getWidgetByName
+ * @param {string} widget Widget
+ * @returns {string} Widget component.
+ */
+const getWidgetByName = (widget) =>
+  typeof widget === 'string'
+    ? config.widgets.widget[widget] || getWidgetDefault()
+    : null;
+
+/**
+ * Get widget by tagged values
+ * @param {object} widgetOptions
+ * @returns {string} Widget component.
+ *
+
+directives.widget(
+    'fieldname',
+    frontendOptions={
+        "widget": 'specialwidget',
+        "version": 'extra'
+    })
+
+ */
+const getWidgetFromTaggedValues = (widgetOptions) =>
+  typeof widgetOptions?.frontendOptions?.widget === 'string'
+    ? config.widgets.widget[widgetOptions.frontendOptions.widget]
+    : null;
+
+/**
+ * Get widget by field's `vocabulary` attribute
+ * @method getWidgetByVocabulary
+ * @param {string} vocabulary Widget
+ * @returns {string} Widget component.
+ */
+const getWidgetByVocabulary = (vocabulary) =>
+  vocabulary && vocabulary['@id']
+    ? config.widgets.vocabulary[
+        vocabulary['@id'].replace(/^.*\/@vocabularies\//, '')
+      ]
+    : null;
+
+/**
+ * Get widget by field's hints `vocabulary` attribute in widgetOptions
+ * @method getWidgetByVocabularyFromHint
+ * @param {string} props Widget props
+ * @returns {string} Widget component.
+ */
+const getWidgetByVocabularyFromHint = (props) =>
+  props.widgetOptions && props.widgetOptions.vocabulary
+    ? config.widgets.vocabulary[
+        props.widgetOptions.vocabulary['@id'].replace(
+          /^.*\/@vocabularies\//,
+          '',
+        )
+      ]
+    : null;
+
+/**
+ * Get widget by field's `choices` attribute
+ * @method getWidgetByChoices
+ * @param {string} choices Widget
+ * @returns {string} Widget component.
+ */
+const getWidgetByChoices = (props) => {
+  if (props.choices) {
+    return config.widgets.choices;
+  }
+
+  if (props.vocabulary) {
+    // If vocabulary exists, then it means it's a choice field in disguise with
+    // no widget specified that probably contains a string then we force it
+    // to be a select widget instead
+    return config.widgets.choices;
+  }
+
+  return null;
+};
+
+/**
+ * Get widget by field's `type` attribute
+ * @method getWidgetByType
+ * @param {string} type Type
+ * @returns {string} Widget component.
+ */
+const getWidgetByType = (type) => config.widgets.type[type] || null;
 
 const hasDescriptionBlock = (blocks = {}) => {
   for (let id in blocks) {
@@ -41,6 +146,16 @@ const hasDescriptionBlock = (blocks = {}) => {
  * ```
  */
 const TextareaWidget = (props) => {
+  const Widget =
+    getWidgetFromTaggedValues(props.widgetOptions) ||
+    getWidgetByName(props.widget) ||
+    getWidgetByChoices(props) ||
+    getWidgetByVocabulary(props.vocabulary) ||
+    getWidgetByVocabularyFromHint(props) ||
+    getWidgetByFactory(props.factory) ||
+    getWidgetByType(props.type) ||
+    getWidgetDefault();
+
   const { id, maxLength, value, onChange, placeholder, formData = {} } = props;
   const [lengthError, setlengthError] = useState('');
 
@@ -56,7 +171,7 @@ const TextareaWidget = (props) => {
     onChange(id, value);
   };
 
-  return (
+  return props.behavior ? (
     <FormFieldWrapper {...props} className="textarea">
       <TextArea
         id={`field-${id}`}
@@ -74,6 +189,8 @@ const TextareaWidget = (props) => {
         </Label>
       )}
     </FormFieldWrapper>
+  ) : (
+    <Widget {...props} />
   );
 };
 
