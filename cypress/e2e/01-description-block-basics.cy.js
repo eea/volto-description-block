cd6affce0d0d48c4ceec872ebd1963792ef226fe
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-07-12 11:47:45 +0300

    Update 01-description-block-basics.cy.js

--- cypress/e2e/01-description-block-basics.cy.js
+++ cypress/e2e/01-description-block-basics.cy.js
@@ -22,7 +22,7 @@ describe('Blocks Tests', () => {
     // Add text with enter key
     cy.get('.documentDescription div[role="textbox"]')
       .click()
-      .type('First line{shift}{enter}Second line{shift}{enter}Third line');
+      .type('First line{shift}{enter}Second line{shift}{enter}Third line{enter}');
 
     // Save
     cy.get('#toolbar-save').click();
