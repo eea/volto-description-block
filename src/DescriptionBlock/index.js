f7f3151cd9680d672be8e4be72910e1dcfd4b491
nileshgulia1 <nileshgulia@gmail.com>
2024-12-18 15:54:43 +0530

    WIP: use Edit from volto-core

--- src/DescriptionBlock/index.js
+++ src/DescriptionBlock/index.js
@@ -1,13 +1,18 @@
-import Edit from './Edit';
+import Edit from '@plone/volto/components/manage/Blocks/Description/Edit';
 import View from './View';
 
 const applyConfig = (config) => {
   config.blocks.blocksConfig.description = {
     ...config.blocks.blocksConfig.description,
-    edit: Edit,
+    edit: (props) => (
+      <div className={config.blocks.blocksConfig.description.className}>
+        <Edit {...props} />
+      </div>
+    ),
     view: View,
     restricted: false,
     className: 'documentDescription',
+    blockHasOwnFocusManagement: false,
   };
 
   // Footnotes
